7cd598361ee35eef0eac56fc6d2ee0f4c4853865
Mrinal Save <47613197+mrinalsave@users.noreply.github.com>
2026-04-16 00:51:34 -0400

    feature/art: create basic masonry responsive grid for art gallery.

---
+++ art/script.js
@@ -0,0 +1,55 @@
+// hardcoded list for first version, will automate later
+const imageList = [
+  "0410.webp",
+  "0510.webp",
+  "0729.webp",
+  "0823.webp",
+  "0620.webp",
+  "0325.webp",
+  "0514-0516.webp",
+  "0726.webp",
+  "0110.webp",
+  "0826.webp",
+  "0124.webp",
+  "0201.webp",
+  "0206.webp",
+  "0211.webp",
+  "0214.webp",
+  "0808.webp",
+  "0320.webp",
+  "0401.webp",
+  "0428-0429.webp",
+  "0830.webp",
+  "0503.webp",
+  "0507.webp",
+  "051024.webp",
+  "0702.webp",
+  "0310.webp",
+  "0615.webp",
+  "0707.webp",
+  "0108.webp"
+];
+
+window.addEventListener('load', () => {
+  const grid = document.querySelector('.grid');
+
+  imageList.forEach(file => {
+    const item = document.createElement('div');
+    item.className = 'grid-item';
+ 
+    const img = document.createElement('img');
+    img.src = `./assets/images/${file}`;
+ 
+    item.appendChild(img);
+    grid.appendChild(item);
+  });
+
+  imagesLoaded(grid, () => {
+    new Masonry(grid, {
+      itemSelector: '.grid-item',
+      columnWidth: '.grid-item',
+      percentPosition: true
+    });
+  });
+});
+ 
